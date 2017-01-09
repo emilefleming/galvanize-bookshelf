19de7f282368bc4a4576c90ba11ae7254da070cf
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-01-09 08:24:43 -0800

    Make server validate POST /users route

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,37 @@ const { camelizeKeys, decamelizeKeys } = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+router.post('/users', (req, _res, next) => {
+  const badReq = function(message) {
+    const err = new Error(message);
+
+    err.output = { statusCode: 400 };
+
+    return next(err);
+  };
+
+  if (!req.body.email) {
+    badReq('Email must not be blank');
+  }
+  if (!req.body.password || req.body.password.length < 8) {
+    badReq('Password must be at least 8 characters long');
+  }
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .then((user) => {
+      if (user) {
+        badReq('Email already exists');
+      }
+      else {
+        next();
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 router.post('/users', (req, res, next) => {
   bcrypt.hash(req.body.password, 12)
     .then((hashed_password) => {
