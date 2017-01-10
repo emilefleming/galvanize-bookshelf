e60dac67592d77c8b0c4c74dc81e059a4a1cb9e4
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-01-10 08:19:36 -0800

    Add token auth and /token route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const bcrypt = require('bcrypt-as-promised');
+const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const express = require('express');
 
@@ -54,6 +55,17 @@ router.post('/users', (req, res, next) => {
         }, '*');
     })
     .then((users) => {
+      const claim = { userId: users[0].id };
+      const token = jwt.sign(claim, process.env.JWT_KEY, {
+        expiresIn: '7 days'
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        secure: router.get('env') === 'production'
+      });
+
       delete users[0].hashed_password;
       res.send(camelizeKeys(users[0]));
     })
